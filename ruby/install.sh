c530d78acac2db1e60f9d47bf1857cd3367191d3
Patrick Burtchaell <patrick@pburtchaell.com>
2018-01-21 12:05:10 -0600

    Fix Ruby output

--- ruby/install.sh
+++ ruby/install.sh
@@ -28,7 +28,7 @@ install_all() {
     "$(gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB)" >> /tmp/rvm-install.log
 
     # Install rvm
-    "$(curl -sSL https://get.rvm.io)" >> ${PWD}/tmp/$LOG_FILEs
+    "$(curl -sSL https://get.rvm.io)" >> ${PWD}/tmp/$LOG_FILE
 
     # Install Ruby
     install_ruby
