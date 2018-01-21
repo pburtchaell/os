a87b2549b70a1198e3767804b555f7d7b22850d5
Patrick Burtchaell <patrick@pburtchaell.com>
2018-01-21 11:55:00 -0600

    Finish setup scripts

--- node/install.sh
+++ node/install.sh
@@ -1,21 +1,46 @@
 #!/bin/sh
-# Last Updated: 2018-01-20
+# Last Updated: 2018-01-21
+# Description: Installs nvm and Node.
+
+LOG_FILE="node-install.log"
+
+# Function: install_node
 # Description: Installs the stable version of Node.
-
-# Node
-if test ! $(which nvm)
-then
+install_node() {
   echo "Installing a stable version of Node..."
-
-  # Load nvm
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
   # Install the stable version.
   nvm install stable > /dev/null 2>&1
 
   # Use the stable version by default.
-  nvm alias default stable >> /tmp/node-install.log
+  nvm alias default stable >> ${PWD}/tmp/node-install.log
+}
+
+# Function: install_all
+# Description: Install both nvm and Mode.
+install_all() {
+  if [ test ! $(which nvm) ]
+  then
+    echo "nvm is already installed."
+
+    install_node
+  else
+    echo "Installing nvm..."
+    "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.s)" >> ${PWD}/tmp/nvm-install.log
+
+    install_node
+  fi
+}
+
+# Load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Only install nvm and Node if the user wants it.
+ANSWER=$(ask_question "nvm and Node")
+
+if [ $ANSWER -eq 1 ]; then
+  install_all
 fi
 
 exit 0
--- setup.sh
+++ setup.sh
@@ -1,11 +1,79 @@
 #!/bin/sh
 # Last Updated: 2018-01-20
-# Description: Installs Homebrew.
+# Description: Runs the setup for everything.
 
+# Make temporary folder for logs
+if [ -d ${PWD}/tmp ]; then rm -rf ${PWD}/tmp; fi
+mkdir ${PWD}/tmp
+
+# Make folder for git repositories
+REPOSITORIES=${HOME}/Repositories
+if [ ! -d ${REPOSITORIES} ]; then mkdir ${REPOSITORIES}; fi
+
+# Function: ask_question
+# Description: Ask a yes/no question and return boolean value based off answer.
+ask_question() {
+  read -p "Would you like to install $1 (y/n): " ANSWER
+
+  case $ANSWER in
+    y | Y | Yes | yes )
+      status=1 # return true
+      break
+      ;;
+    n | N | No | no)
+      status=0 # return false
+      break
+      ;;
+    * )
+      echo "Invalid answer. Please enter either \"y/yes\" or \"n/no\""
+      ask_question "$1" ANSWER
+      break
+      ;;
+  esac
+
+  echo $status
+  return
+}
+
+# Function: install_log
+# Description: Log an install message.
+install_log() {
+  echo "Installing $1..." | tee -a ${PWD}/tmp/$2
+
+  return
+}
+
+# Function: already_install_log
+# Description: Log an already installed message.
+already_install_log() {
+  echo "$1 is already installed." | tee -a ${PWD}/tmp/$2
+}
+
+# Function: log
+# Description: Log a message to the console and to a file
+log() {
+  echo $1 | tee -a ${PWD}/tmp/$2
+}
+
+# Export functions
+export -f ask_question
+export -f install_log
+export -f already_install_log
+export -f log
+
+# Install development and design tools
 sh brew/install.sh
 sh node/install.sh
 sh node/setup.sh
+sh ruby/install.sh
 sh ruby/setup.sh
-sh macos/setup.sh
+
+# Set macOS defaults
+log "Installing macOS defaults..." "macos-setup.log"
+MACOS_ANSWER=$(ask_question "macOS defaults")
+
+if [ $MACOS_ANSWER -eq 1 ]; then
+  sh macos/setup.sh
+fi
 
 exit 0
