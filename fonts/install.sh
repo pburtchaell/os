306dd073c90719f7477df8d4bc9e52926182d93d
Patrick Burtchaell <pburtchaell@fb.com>
2018-10-14 09:42:57 -0500

    Add fonts install script

---
+++ fonts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Last Updated: 2018-10-13
+# Description:
+
+LOG_FILE="fonts-install.log"
+
+# todo: ask for private github repo with fonts
+# todo: ask for private access key
+# todo: clone private github repo with my fonts
+cp -P "./Operator Mono/*" "/Library/Fonts"
